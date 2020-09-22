1fde39ff82411e421758b92f1fdbf7dbed423f2c
Ryan Muinos <ryanemuinos@outlook.com>
2020-09-22 00:10:46 -0700

    Update README

--- Data Structures/Data_StructuresApp.swift
+++ Data Structures/Data_StructuresApp.swift
@@ -10,8 +10,56 @@ import SwiftUI
 @main
 struct Data_StructuresApp: App {
     var body: some Scene {
-        WindowGroup {
-            Home(viewModel: DataStructureManagerMV()).environmentObject(UIStateModel())
+        WindowGroup { () -> Home in
+            let modelView = DataStructureManagerMV()
+            let BST1: BST = BST()
+            BST1.insert(value: 3)
+            BST1.insert(value: 5)
+            BST1.insert(value: 2)
+            BST1.insert(value: 1)
+            BST1.insert(value: 10)
+            BST1.insert(value: 4)
+            BST1.insert(value: 7)
+            BST1.insert(value: 8)
+            modelView.insert(DS: BST1)
+            let BST2: BST = BST()
+            BST2.insert(value: 3)
+            BST2.insert(value: 1)
+            BST2.insert(value: 2)
+            
+            BST2.insert(value: 10)
+            BST2.insert(value: 7)
+            BST2.insert(value: 8)
+            modelView.insert(DS: BST2)
+            
+            
+            let BST3: BST = BST()
+            BST3.insert(value: 3)
+            BST3.insert(value: 1)
+            BST3.insert(value: 2)
+            
+            modelView.insert(DS: BST3)
+            
+            
+            
+            let BST4: BST = BST()
+            BST4.insert(value: 3)
+            BST4.insert(value: 1)
+            BST4.insert(value: 2)
+            BST4.insert(value: 10)
+            BST4.insert(value: 7)
+            BST4.insert(value: 8)
+            modelView.insert(DS: BST4)
+            
+            
+            let BST5: BST = BST()
+            BST5.insert(value: 3)
+            BST5.insert(value: 1)
+            
+            BST5.insert(value: 7)
+            BST5.insert(value: 8)
+            modelView.insert(DS: BST5)
+            return Home(viewModel: modelView).environmentObject(UIStateModel()) as! Home
             
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DataStructuresIOSApp
+![Interface](https://user-images.githubusercontent.com/58897872/93853006-e90d1080-fc67-11ea-85eb-15efb817bee3.png)
+App used to visualize various Data Structures and their functions
+suc as Binary Tree and Linked List
